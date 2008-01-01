2b9b5003b5533b402ab6d815143805fe1b4a5d83
nedwidek <nedwidek@f711f47a-2549-4967-850e-05643b90d7f4>
2008-01-01 16:02:25 +0000

    Tagging out 0.8.1
    Incorporates style changes from [UTAC] Eskiso.
    
    git-svn-id: http://subversion.assembla.com/svn/hackbuster@17 f711f47a-2549-4967-850e-05643b90d7f4

---
+++ php/tags/version-0.8.1/head.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>BF2 Name Hack Buster</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+<script language="JavaScript">
+<!--
+
+function updateStatus(text) {
+    statusArea = document.getElementById('statusArea');
+
+    statusArea.innerHTML = text;
+}
+
+-->
+</script>
+</head>
+<body> 
+
+
+<table border="0" width="100%">
+<tr>
+<td><a href="index.php">Generate Report</a></td>
+<td><a href="saved.php">Saved Reports</a></td>
+<td><a href="shame.php">Wall of Shame</a></td>
+<td><a href="about.php">About</a></td>
+</tr>
+</table>
